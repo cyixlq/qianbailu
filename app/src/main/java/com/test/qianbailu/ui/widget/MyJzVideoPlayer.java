ef5cdb6ddc02b6c8eacc79efe8a4bf722afe4e73
cyixlq <2421597157@qq.com>
2020-01-17 15:13:38 +0800

    新增版本更新，调整倍速弹出框细节

--- app/src/main/java/com/test/qianbailu/ui/widget/MyJzVideoPlayer.java
+++ app/src/main/java/com/test/qianbailu/ui/widget/MyJzVideoPlayer.java
@@ -3,21 +3,26 @@ package com.test.qianbailu.ui.widget;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AlertDialog;
 
 import com.test.qianbailu.R;
 
+import cn.jzvd.JZDataSource;
+import cn.jzvd.Jzvd;
 import cn.jzvd.JzvdStd;
+import top.cyixlq.core.utils.DisplayUtil;
 
 public class MyJzVideoPlayer extends JzvdStd {
 
     protected TextView tvSpeed;
-    private float currentSpeed = 1.0f;
-    private SpeedDialogFragment speedDialog;
+    private int currentSpeedIndex = 1;
+    private AlertDialog speedDialog;
 
     public MyJzVideoPlayer(Context context) {
         super(context);
@@ -34,20 +39,11 @@ public class MyJzVideoPlayer extends JzvdStd {
         tvSpeed.setOnClickListener(this);
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     public void setScreenNormal() {
         super.setScreenNormal();
         //tvSpeed.setVisibility(View.GONE);
         titleTextView.setVisibility(View.INVISIBLE);
-
-        if (jzDataSource.objects == null) {
-            jzDataSource.objects = new Object[]{2};
-            currentSpeed = 1.0f;
-        } else {
-            currentSpeed = (float) jzDataSource.objects[0];
-        }
-        tvSpeed.setText(currentSpeed + "x");
     }
 
     @Override
@@ -55,6 +51,33 @@ public class MyJzVideoPlayer extends JzvdStd {
         super.setScreenFullscreen();
         tvSpeed.setVisibility(View.VISIBLE);
         titleTextView.setVisibility(VISIBLE);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void setUp(JZDataSource jzDataSource, int screen) {
+        super.setUp(jzDataSource, screen);
+        if (jzDataSource.objects == null) {
+            jzDataSource.objects = new Object[]{2};
+            currentSpeedIndex = 1;
+        } else {
+            currentSpeedIndex = (int) jzDataSource.objects[0];
+        }
+        tvSpeed.setText(getSpeedFromIndex(currentSpeedIndex) + "x");
+    }
+
+    private void setWindow(boolean isFull) {
+        final Window window = speedDialog.getWindow();
+        if (window == null) return;
+        if (isFull) {
+            window.getAttributes().width = DisplayUtil.INSTANCE.dp2px(300f);
+            window.getAttributes().height = WindowManager.LayoutParams.MATCH_PARENT;
+            window.setGravity(Gravity.END);
+        } else {
+            window.getAttributes().width = WindowManager.LayoutParams.MATCH_PARENT;
+            window.getAttributes().height = WindowManager.LayoutParams.WRAP_CONTENT;
+            window.setGravity(Gravity.BOTTOM);
+        }
     }
 
 
@@ -65,19 +88,24 @@ public class MyJzVideoPlayer extends JzvdStd {
         final int id = v.getId();
         if (id == R.id.tv_speed) {//0.5 1.0 1.25 1.5 2.0
             if (speedDialog == null) {
-                speedDialog = new SpeedDialogFragment();
-                speedDialog.setOnSpeedSelectListener(speed -> {
-                    currentSpeed = speed;
-                    mediaInterface.setSpeed(currentSpeed);
-                    tvSpeed.setText(currentSpeed + "x");
-                    jzDataSource.objects[0] = currentSpeed;
-                    speedDialog.dismiss();
-                });
+                final String[] speed = new String[]{"0.5倍", "1.0倍", "1.25倍", "1.5倍", "2.0倍"};
+                speedDialog = new AlertDialog.Builder(getContext())
+                        .setTitle("倍速选择")
+                        .setSingleChoiceItems(speed, currentSpeedIndex, (dialog, which) -> {
+                            currentSpeedIndex = which;
+                            mediaInterface.setSpeed(currentSpeedIndex);
+                            tvSpeed.setText(getSpeedFromIndex(currentSpeedIndex) + "x");
+                            jzDataSource.objects[0] = currentSpeedIndex;
+                            dialog.dismiss();
+                        }).create();
+                if (speedDialog.getWindow() != null) {
+                    View decorView = speedDialog.getWindow().getDecorView();
+                    decorView.setPadding(0, 0, 0, 0);
+                    speedDialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_speed_dialog);
+                }
             }
-            if (getContext() instanceof FragmentActivity) {
-                FragmentManager manager = ((FragmentActivity) getContext()).getSupportFragmentManager();
-                speedDialog.show(manager, "speed");
-            }
+            setWindow(screen == SCREEN_FULLSCREEN);
+            speedDialog.show();
         }
     }
 
@@ -86,4 +114,18 @@ public class MyJzVideoPlayer extends JzvdStd {
         return R.layout.layout_my_jz_video_player;
     }
 
+    private float getSpeedFromIndex(int index) {
+        switch (index) {
+            case 0:
+                return 0.5f;
+            case 2:
+                return 1.25f;
+            case 3:
+                return 1.5f;
+            case 4:
+                return 2.0f;
+            default:
+                return 1.0f;
+        }
+    }
 }
